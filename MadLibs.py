26438d0c01bb0821fad7479ed8101b72dc761793
Al <nas.ali2001@gmail.com>
2020-09-17 12:07:20 -0400

    Mad Lib With Python Added.

---
+++ MadLibs.py
@@ -0,0 +1,18 @@
+"""
+Just a REALLY BASIC MADLIBS
+LET'S JUST HAVE FUN :)
+"""
+color = input('COLOR: ')
+flower = input('Flower: ')
+celeb = input('Celebrity: ')
+celeb2 = input('Another Celebrity: ')
+obj = input('An Object: ')
+body = input('A Body Part: ')
+body2 = input('Another Body Part: ')
+
+print('Roses are {color}'.format(color=color))
+print('{flower} are Blue'.format(flower=flower))
+print("He's for {celeb} And not for {celeb2}".format(celeb=celeb, celeb2=celeb2))
+print('By a chance you take my {obj}'.format(obj=obj))
+print("I'll take my {body} And smash your {body2}".format(body=body, body2=body2))
+print('Hope You had some Fun \"Thumbs Up\" <3')
